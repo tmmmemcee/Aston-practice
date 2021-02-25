2e27b86e642870de065b199bbef60f5ead50b2df
tmmmemcee <tmmmemcee@gmail.com>
2021-02-25 12:37:03 -0600

    delete all

--- src/main/java/com/astontech/virl/student/repositories/MenteeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.astontech.virl.student.repositories;
-
-import com.astontech.virl.student.domain.Mentee;
-import org.springframework.data.repository.CrudRepository;
-
-public interface MenteeRepository extends CrudRepository<Mentee,String> {
-    Mentee findAllByName(String name);
-}
